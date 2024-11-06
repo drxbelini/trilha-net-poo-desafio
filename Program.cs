eca261ee8ce2ada9df9987a3df06fc75e53c449d
Diego Belini <drxbelini@gmail.com>
2024-11-06 09:22:53 -0300

    Finalização do desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia( numero:"(14)981721223", modelo:"A21", imei:"N123456", memoria:6400);
+Smartphone iphone = new Iphone(numero:"(14)981317751", modelo:"iphone11", imei:"IPH123456789", 12800);
+
+
+Console.WriteLine("SmartPhone Nokia");
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("SmartPhone Iphone");
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegran");
+
+
